50b17a9bf46437d785ce3d97c63df1172e3512d3
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-03-21 08:33:59 +0000

    refactor(player): set player ship colour

--- src/entities/player.lua
+++ src/entities/player.lua
@@ -27,59 +27,6 @@ function Player.init(thrust)
     Player.thrust = thrust or 500
 end
 
-function Player.draw(shield_time, collision_time)
-    love.graphics.push()
-
-    love.graphics.translate(Player.x, Player.y)
-    love.graphics.rotate(Player.rotation)
-
-    if collision_time > 0 then
-        Player.is_thrusting = false
-        audio.stop_thrusters()
-
-        local collision_colour_inner = colours.PLAYER.COLLISION_1
-        collision_colour_inner[4] = collision_time / 3
-
-        local collision_colour_outer = colours.PLAYER.COLLISION_2
-        collision_colour_outer[4] = collision_time / 3
-
-        love.graphics.setColor(collision_colour_outer)
-        love.graphics.circle('fill', 0, 0, Player.size * 1.25)
-
-        love.graphics.setColor(collision_colour_inner)
-        love.graphics.circle('fill', 0, 0, Player.size / 2)
-
-        love.graphics.setColor(colours.UI.COLOUR)
-    else
-        if shield_time > 0 then
-            local shield_colour = colours.PLAYER.SHIELD
-            shield_colour[4] = shield_time / 3
-
-            love.graphics.setColor(shield_colour)
-            love.graphics.circle('line', 0, 0, Player.size * 1.5)
-            love.graphics.setColor(colours.UI.COLOUR)
-        end
-
-        love.graphics.polygon('line', 0, -Player.size,
-            -Player.size / 1.5, Player.size / 1.5,
-            Player.size / 1.5, Player.size / 1.5)
-
-        if Player.is_thrusting then
-            audio.play_thrusters()
-            love.graphics.setColor(colours.PLAYER.SHIP_THRUST)
-            love.graphics.polygon('fill', 0, Player.size * 1.5,
-                -Player.size / 2.5, Player.size / 2.5 + 5,
-                Player.size / 2.5, Player.size / 2.5 + 5)
-        else
-            audio.stop_thrusters()
-        end
-    end
-
-    love.graphics.setColor(colours.UI.COLOUR)
-
-    love.graphics.pop()
-end
-
 function Player.update(dt)
     -- Rotation
     if love.keyboard.isDown('left') then
@@ -116,6 +63,61 @@ function Player.update(dt)
     -- Screen wrapping
     Player.x = Player.x % love.graphics.getWidth()
     Player.y = Player.y % love.graphics.getHeight()
+end
+
+function Player.draw(shield_time, collision_time)
+    love.graphics.push()
+
+    love.graphics.translate(Player.x, Player.y)
+    love.graphics.rotate(Player.rotation)
+
+    if collision_time > 0 then
+        Player.is_thrusting = false
+        audio.stop_thrusters()
+
+        local collision_colour_inner = colours.PLAYER.COLLISION_1
+        collision_colour_inner[4] = collision_time / 3
+
+        local collision_colour_outer = colours.PLAYER.COLLISION_2
+        collision_colour_outer[4] = collision_time / 3
+
+        love.graphics.setColor(collision_colour_outer)
+        love.graphics.circle('fill', 0, 0, Player.size * 1.25)
+
+        love.graphics.setColor(collision_colour_inner)
+        love.graphics.circle('fill', 0, 0, Player.size / 2)
+
+        love.graphics.setColor(colours.UI.COLOUR)
+    else
+        if shield_time > 0 then
+            local shield_colour = colours.PLAYER.SHIELD
+            shield_colour[4] = shield_time / 3
+
+            love.graphics.setColor(shield_colour)
+            love.graphics.circle('line', 0, 0, Player.size * 1.5)
+            love.graphics.setColor(colours.UI.COLOUR)
+        end
+
+        love.graphics.setColor(colours.PLAYER.SHIP)
+        love.graphics.polygon('line', 0, -Player.size,
+            -Player.size / 1.5, Player.size / 1.5,
+            Player.size / 1.5, Player.size / 1.5)
+        love.graphics.setColor(colours.UI.COLOUR)
+
+        if Player.is_thrusting then
+            audio.play_thrusters()
+            love.graphics.setColor(colours.PLAYER.SHIP_THRUST)
+            love.graphics.polygon('fill', 0, Player.size * 1.5,
+                -Player.size / 2.5, Player.size / 2.5 + 5,
+                Player.size / 2.5, Player.size / 2.5 + 5)
+        else
+            audio.stop_thrusters()
+        end
+    end
+
+    love.graphics.setColor(colours.UI.COLOUR)
+
+    love.graphics.pop()
 end
 
 function Player.handle_key_press(key)

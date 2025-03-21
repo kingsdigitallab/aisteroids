a35f666f39b6db65689b6777acdb4b08a3f9a0a7
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-03-21 16:36:57 +0000

    feat(audio): add new sound effects for ship explosion and asteroid hit

--- src/utils/audio.lua
+++ src/utils/audio.lua
@@ -3,13 +3,17 @@ local love = require('love')
 local Audio = {
     bgm = love.audio.newSource('assets/music/aisteroids.ogg', 'stream'),
     laser = love.audio.newSource('assets/sounds/laser.ogg', 'static'),
-    thrusters = love.audio.newSource('assets/sounds/thrusters.ogg', 'static')
+    thrusters = love.audio.newSource('assets/sounds/thrusters.ogg', 'static'),
+    explosion = love.audio.newSource('assets/sounds/explosion.ogg', 'static'),
+    asteroid_hit = love.audio.newSource('assets/sounds/asteroid_hit.ogg', 'static')
 }
 
 function Audio.play_bgm()
     Audio.bgm:play()
     Audio.bgm:setLooping(true)
     Audio.bgm:setVolume(0.2)
+
+    Audio.explosion:setVolume(0.5)
 end
 
 function Audio.stop_bgm()
@@ -32,6 +36,14 @@ end
 
 function Audio.stop_thrusters()
     Audio.thrusters:stop()
+end
+
+function Audio.play_explosion()
+    Audio.explosion:play()
+end
+
+function Audio.play_asteroid()
+    Audio.asteroid:play()
 end
 
 return Audio

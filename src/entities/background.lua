71eb2fd29e0f8a0cd58f5d59200360b8661b43d0
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-03-23 12:04:22 +0000

    refactor(background): add random opacity to the background stars

--- src/entities/background.lua
+++ src/entities/background.lua
@@ -1,5 +1,7 @@
 local love = require('love')
 
+local colours = require('src.utils.colours')
+
 local Background = {
     stars = {}
 }
@@ -11,6 +13,7 @@ function Background.init()
         table.insert(Background.stars, {
             x = love.math.random(0, love.graphics.getWidth()),
             y = love.math.random(0, love.graphics.getHeight()),
+            colour = { colours.UI.COLOUR[1], colours.UI.COLOUR[2], colours.UI.COLOUR[3], math.random(0.25, 0.75) },
             size = love.math.random(0.5, 1.5),
         })
     end
@@ -19,6 +22,7 @@ end
 function Background.update(dt)
     for _, star in ipairs(Background.stars) do
         star.y = star.y + dt * 4
+
         if star.y > love.graphics.getHeight() then
             star.y = 0
         end
@@ -27,7 +31,9 @@ end
 
 function Background.draw()
     for _, star in ipairs(Background.stars) do
+        love.graphics.setColor(star.colour)
         love.graphics.circle('fill', star.x, star.y, star.size)
+        love.graphics.setColor(colours.UI.COLOUR)
     end
 end
 
